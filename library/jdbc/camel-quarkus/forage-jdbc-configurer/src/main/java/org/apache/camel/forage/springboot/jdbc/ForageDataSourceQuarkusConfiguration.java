548cb46c16bb26cbc94c81062ed3e1e359fc2b4f
Jiri Ondrusek <ondrusek.jiri@gmail.com>
2025-09-18 15:58:14 +0200

    wip

---
+++ library/jdbc/camel-quarkus/forage-jdbc-configurer/src/main/java/org/apache/camel/forage/springboot/jdbc/ForageDataSourceQuarkusConfiguration.java
@@ -0,0 +1,56 @@
+package org.apache.camel.forage.springboot.jdbc;
+
+import io.agroal.api.AgroalDataSource;
+import io.agroal.springframework.boot.AgroalDataSourceAutoConfiguration;
+import jakarta.annotation.PostConstruct;
+import java.util.List;
+import java.util.ServiceLoader;
+import org.apache.camel.forage.core.common.ServiceLoaderHelper;
+import org.apache.camel.forage.core.jdbc.DataSourceProvider;
+import org.apache.camel.forage.jdbc.factory.DataSourceFactoryConfig;
+import org.apache.camel.forage.jdbc.factory.MultiDataSourceConfig;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+//@AutoConfigureBefore({DataSourceAutoConfiguration.class, AgroalDataSourceAutoConfiguration.class})
+@ApplicationScope
+public class ForageDataSourceQuarkusConfiguration implements ConfigSource {
+
+//    @Inject
+//    private
+
+    private static final Map<String, String> configuration = new HashMap<>();
+
+//    public void afterCobstruct() {
+////        initialize config properties based on DataSourceFactoryConfig
+//    }
+
+public void init() {
+    ConfigStore.getInstance()
+}
+
+    @Override
+    public int getOrdinal() {
+        return 275;
+    }
+
+    @Override
+    public Set<String> getPropertyNames() {
+        return configuration.keySet();
+    }
+
+    @Override
+    public String getValue(final String propertyName) {
+        return configuration.get(propertyName);
+    }
+
+    @Override
+    public String getName() {
+        return InMemoryConfigSource.class.getSimpleName();
+    }
+}

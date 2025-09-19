3b95987249576ef1b3fbda79121d938854103376
Jiri Ondrusek <ondrusek.jiri@gmail.com>
2025-09-19 09:19:43 +0200

    wip

--- library/jdbc/camel-quarkus/forage-jdbc-configurer/src/main/java/org/apache/camel/forage/quarkus/jdbc/ForageDataSourceQuarkusConfiguration.java
+++ library/jdbc/camel-quarkus/forage-jdbc-configurer/src/main/java/org/apache/camel/forage/quarkus/jdbc/ForageDataSourceQuarkusConfiguration.java
@@ -1,12 +1,18 @@
-package org.apache.camel.forage.springboot.jdbc;
+package org.apache.camel.forage.quarkus.jdbc;
 
 import io.agroal.api.AgroalDataSource;
 import io.agroal.springframework.boot.AgroalDataSourceAutoConfiguration;
 import jakarta.annotation.PostConstruct;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.ServiceLoader;
+import java.util.Set;
+
 import org.apache.camel.forage.core.common.ServiceLoaderHelper;
 import org.apache.camel.forage.core.jdbc.DataSourceProvider;
+import org.apache.camel.forage.core.util.config.ConfigStore;
 import org.apache.camel.forage.jdbc.factory.DataSourceFactoryConfig;
 import org.apache.camel.forage.jdbc.factory.MultiDataSourceConfig;
 import org.springframework.beans.factory.BeanFactory;
@@ -18,7 +24,6 @@ import org.springframework.context.annotation.Configuration;
 
 //@Configuration
 //@AutoConfigureBefore({DataSourceAutoConfiguration.class, AgroalDataSourceAutoConfiguration.class})
-@ApplicationScope
 public class ForageDataSourceQuarkusConfiguration implements ConfigSource {
 
 //    @Inject
@@ -26,13 +31,9 @@ public class ForageDataSourceQuarkusConfiguration implements ConfigSource {
 
     private static final Map<String, String> configuration = new HashMap<>();
 
-//    public void afterCobstruct() {
-////        initialize config properties based on DataSourceFactoryConfig
-//    }
-
-public void init() {
-    ConfigStore.getInstance()
-}
+    static {
+        configuration.put("my.prop", "1234");
+    }
 
     @Override
     public int getOrdinal() {

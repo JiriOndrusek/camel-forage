c57bf5a34688f674c7da8903e8abc637e1b21d53
Jiri Ondrusek <ondrusek.jiri@gmail.com>
2025-10-14 11:03:20 +0200

    wip

--- integrationTests/jdbc/src/test/java/org/apache/camel/forage/jdbc/CamelJBangIT.java
+++ integrationTests/jdbc/src/test/java/org/apache/camel/forage/jdbc/CamelJBangIT.java
@@ -20,63 +20,22 @@ import org.citrusframework.TestActionSupport;
 import org.citrusframework.annotations.CitrusTest;
 import org.citrusframework.spi.Resources;
 import org.citrusframework.testng.TestNGCitrusSupport;
-import org.citrusframework.util.TestUtils;
-import org.testng.SkipException;
 import org.testng.annotations.Test;
 
 public class CamelJBangIT extends TestNGCitrusSupport implements TestActionSupport {
 
-    @Test
-    @CitrusTest(name = "RunIntegration_SourceCode_IT")
-    public void runIntegrationWithSourceCodeIT() {
-
-        if (!TestUtils.isNetworkReachable()) {
-            throw new SkipException(
-                    "Test skipped because network is not reachable. We are probably running behind a proxy and JBang download is not possible.");
-        }
-
-        //        given(doFinally().actions(
-        //                catchException().actions(camel().jbang().stop().integration("hello"))
-        //        ));
-
-        when(camel().jbang()
-                .run(
-                        "hello",
-                        """
-                - from:
-                    uri: "timer:tick"
-                    parameters:
-                      period: "1000"
-                      includeMetadata: true
-                    steps:
-                      - setBody:
-                          simple: "{{greeting}} #${header.CamelTimerCounter}"
-                      - transform:
-                          simple: "${body.toUpperCase()}"
-                      - to: "log:info"
-                """)
-                .withSystemProperty("greeting", "Hello Camel"));
-
-        then(camel().jbang().verify().integration("hello").waitForLogMessage("HELLO CAMEL #10"));
-    }
+    //todo start container
 
     @Test
     @CitrusTest(name = "RunIntegration_Resource_IT")
-    public void runIntegrationWithResourceIT() {
-
-        if (!TestUtils.isNetworkReachable()) {
-            throw new SkipException(
-                    "Test skipped because network is not reachable. We are probably running behind a proxy and JBang download is not possible.");
-        }
-
-        //        given(doFinally()
-        //                .actions(catchException().actions(camel().jbang().stop().integration("route"))));
+    public void singleIT() {
 
         when(camel().jbang()
-                .run()
-                .integration(Resources.fromClasspath("route.yaml", CamelJBangIT.class))
-                .withEnv("GREETING", "Hello Camel"));
+                .custom("forage run")
+                .integration(Resources.fromClasspath("route.camel.yaml", CamelJBangIT.class))
+                .addResource(Resources.fromClasspath("forage-datasource-factory.properties", CamelJBangIT.class))
+                .withArg("--runtime", "quarkus"));
 
-        then(camel().jbang().verify().integration("route").waitForLogMessage("HELLO CAMEL #10"));
+        then(camel().jbang().verify().integration("route-2362").waitForLogMessage("HELLO CAMEL #10"));
     }
 }

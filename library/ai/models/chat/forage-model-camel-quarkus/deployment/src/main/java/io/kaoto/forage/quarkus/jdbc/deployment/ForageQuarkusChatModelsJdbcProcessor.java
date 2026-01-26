0ca74fe3963a21ce4c6bdd0b29b9e2b0dd124656
Jiri Ondrusek <ondrusek.jiri@gmail.com>
2026-01-26 15:59:21 +0100

    Support of Quarkus chat models

---
+++ library/ai/models/chat/forage-model-camel-quarkus/deployment/src/main/java/io/kaoto/forage/quarkus/jdbc/deployment/ForageQuarkusChatModelsJdbcProcessor.java
@@ -0,0 +1,42 @@
+package io.kaoto.forage.quarkus.jdbc.deployment;
+
+import dev.langchain4j.model.ModelProvider;
+import dev.langchain4j.model.chat.ChatModel;
+import io.quarkus.arc.deployment.BeanDiscoveryFinishedBuildItem;
+import io.quarkus.deployment.annotations.BuildProducer;
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.annotations.ExecutionTime;
+import io.quarkus.deployment.annotations.Record;
+import org.apache.camel.quarkus.core.deployment.spi.CamelRuntimeBeanBuildItem;
+
+//
+//@ForageFactory(
+//        value = "DataSource (Quarkus)",
+//        components = {"camel-sql", "camel-jdbc"},
+//        description = "Native JDBC DataSource for Quarkus with compile-time optimization and repository support",
+//        type = FactoryType.DATA_SOURCE,
+//        autowired = true,
+//        configClass = DataSourceFactoryConfig.class,
+//        variant = FactoryVariant.QUARKUS)
+public class ForageQuarkusChatModelsJdbcProcessor {
+
+    @Record(ExecutionTime.RUNTIME_INIT)
+    @BuildStep
+        void quarkusChatModels(QuarkusForageChatModelsRecorder recorder,
+                                BuildProducer<CamelRuntimeBeanBuildItem> camelRuntimeBean,
+                                BeanDiscoveryFinishedBuildItem discovery) {
+
+        discovery.beanStream()
+                .filter(bean -> bean.getTypes().contains(ChatModel.class))
+                .forEach(bean -> {
+
+
+                    camelRuntimeBean.produce(
+                            new CamelRuntimeBeanBuildItem(
+                                    bean.getName() + "ForageModelProvider",
+                                    ModelProvider.class.getName(),
+                                    recorder.createModelProvider(bean.getName())))
+                                    .done();
+                });
+    }
+}
